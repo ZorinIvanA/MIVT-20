494abcac8a117967ce475fc976f235e4a76f5390
ZorinIvanA <zorin.ivan@outlook.com>
2021-10-21 10:08:35 +0300

    new-service

---
+++ Books/Books/Books.Books/Infrastructure/DTO/BookDTO.cs
@@ -0,0 +1,25 @@
+﻿using Books.Books.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.Books.Infrastructure.DTO
+{
+    public class BookDTO
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string author { get; set; }
+
+        public Book GetBook()
+        {
+            return new Book 
+            {
+                Author = this.author,
+                BookName = this.name,
+                Id = this.id
+            };
+        }
+    }
+}
